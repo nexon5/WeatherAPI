3b039b22c39f25da659fa3de86145aecb11ffcdc
nexon5 <dm.lychahin@gmail.com>
2021-09-01 09:43:29 +0300

    2109commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 let btnPol = document.querySelector(".poltava");
 let btnKha = document.querySelector(".kharkov");
 let btnKiev = document.querySelector(".kiev");
+let btnMsk = document.querySelector(".msk");
 
 
 
@@ -10,19 +11,11 @@ function ShowWeather(string1) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${string1}&appid=3d3c50d2cc6960668ccaab7656cc36f3`)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
-
-            console.log(data);
-
+            console.log(data)
             document.querySelector(".city-name").textContent = `${data.name}, ${data.sys.country}`;
             document.querySelector(".temp").innerHTML = `${Math.round(+data.main.temp - 274)}&deg`;
             document.querySelector(".weath-icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             document.querySelector(".weath").textContent = data.weather[0].description;
-
-            console.log("Нажата" + string1)
-
-
-
-
         })
         .catch(function () {
             //catch any errors
@@ -37,4 +30,5 @@ ShowWeather("Poltava, UA");
 btnPol.addEventListener('click', function () { ShowWeather("Poltava, UA"); });
 btnKha.addEventListener('click', function () { ShowWeather("Kharkiv, UA"); });
 btnKiev.addEventListener('click', function () { ShowWeather("Kiev, UA"); });
+btnMsk.addEventListener('click', function () { ShowWeather("Moscow, RU"); });
 
